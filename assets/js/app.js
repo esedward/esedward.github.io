186a451720e822f5f7522f758d9b176ccca2e388
Erin Edward <esedward@uwaterloo.ca>
2022-06-07 15:16:17 -0400

    media queries

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,13 @@ scrollUp.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// Nav hamburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
